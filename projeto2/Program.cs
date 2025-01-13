a2c4aeda50b9b51672191203a3ccffab3e1847ba
IZABELLA SILVA SANTOS <izabella.ssantos5@senacsp.edu.br>
2025-01-13 20:05:04 -0300

    first commit

---
+++ projeto2/Program.cs
@@ -0,0 +1,6 @@
+﻿// ex.02  - Escreva um programa que pergunte o nome do usuário, armazene esse nome numa variável name e apresente uma mensagem de boas-vindas exibindo o nome de usuásio
+
+Console.WriteLine("Informe o seu nome: ");
+string name =  Console.ReadLine();
+
+Console.WriteLine("Seja bem-vindo(a)" + name);
